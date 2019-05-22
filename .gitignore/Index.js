6bb75d3b6abbede3644bbb1dd1dbfcbff444c428
onhamzaytb <50212984+onhamzaytb@users.noreply.github.com>
2019-05-22 19:16:17 +0200

    Update and rename .gitignore/Index.js to  .gitignore/Index.js

--- .gitignore/Index.js
+++ .gitignore/Index.js
@@ -5,5 +5,8 @@ client.on('ready', () => {
     console.log("je suis connecter" + client.user.tag)
 })
 
-
+client . on ('message', msg => { 
+  if (msg . content === 'ping') { 
+    msg . reply ('Pong!') 
+      
 client.login('NTY1MjMxMTQ5MzI5NDE2MjIy.XOU8eA.q5dH4CybqPNNNMsjoxWGsGYilZI')
